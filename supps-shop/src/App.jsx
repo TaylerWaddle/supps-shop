9f1d8adc86d33d2aa6be41f8a2641067b99d9796
Tayler Waddle <taylerwaddle@gmail.com>
2024-08-04 20:57:36 -0600

    created home route

--- supps-shop/src/App.jsx
+++ supps-shop/src/App.jsx
@@ -1,14 +1,15 @@
-import "./App.css";
+import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 
 import Navigation from "./components/navigation/navigation.component";
+import Home from "./routes/home/home.component";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
-        {/* <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
+        <Route index element={<Home />} />
+        {/* <Route path="shop/*" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
         <Route path="checkout" element={<Checkout />} /> */}
       </Route>
---
+++ supps-shop/src/routes/home/home.component.jsx
@@ -0,0 +1,7 @@
+import "./home.cstyles.scss";
+
+const Home = () => {
+  return <div>home page</div>;
+};
+
+export default Home;
