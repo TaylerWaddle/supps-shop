dd0729fb742a9ce31b11021160caac5c98f5ac4a
Tayler Waddle <taylerwaddle@gmail.com>
2024-08-06 12:50:18 -0600

    deleted comment

--- supps-shop/src/App.jsx
+++ supps-shop/src/App.jsx
@@ -9,27 +9,11 @@ function App() {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        {/* <Route path="shop/*" element={<Shop />} />
+      </Route>
+      {/* <Route path="shop/*" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
         <Route path="checkout" element={<Checkout />} /> */}
-      </Route>
     </Routes>
-
-    // <h1>Pure Apex</h1>
-    // <h4>Power Supplements</h4>
-    // <h1>Popular Products</h1>
-    // <div>
-    //   <h3>Joker Pre-Workout</h3>
-    //   <p>
-    //     Unleash your inner beast with Joker Pre Workout! Maximize energy,
-    //     focus, and endurance with this powerful blend. Experience explosive
-    //     power and razor-sharp concentration with every scoop. Elevate your
-    //     performance and dominate your training sessions. Get ready to laugh at
-    //     your limits!
-    //   </p>
-    //   <img></img>
-    // </div>
-    // <h1>About Us</h1>
   );
 }
 
