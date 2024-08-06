9f5229d1bcbdb2848a1d40a20adbc73c6bff4edd
Tayler Waddle <taylerwaddle@gmail.com>
2024-08-06 15:14:55 -0600

    finsihed hero vid and hero content

--- supps-shop/src/routes/home/home.component.jsx
+++ supps-shop/src/routes/home/home.component.jsx
@@ -7,6 +7,14 @@ const Home = () => {
     <>
       <div className="hero-container">
         <video className="hero-vid" src={heroVidZoomed} autoPlay muted loop />
+        <div className="hero-content">
+          <h1 className="hero-title">PURE</h1>
+          <h1 className="hero-title">APEX</h1>
+          <span className="hero-subtitle">POWER SUPPLEMENTS</span>
+        </div>
+        <div className="button-container">
+          <button className="hero-button">Shop Now</button>
+        </div>
       </div>
     </>
   );
