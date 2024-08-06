963f0696830445b482b88bc6dccb348823fda301
Tayler Waddle <taylerwaddle@gmail.com>
2024-08-06 13:51:31 -0600

    updated hero video

--- supps-shop/src/routes/home/home.component.jsx
+++ supps-shop/src/routes/home/home.component.jsx
@@ -1,9 +1,13 @@
 import "./home.styles.scss";
 
+import heroVidZoomed from "../../assets/hero-vid-zoomed.mp4";
+
 const Home = () => {
   return (
     <>
-      <div className="hero-container"></div>
+      <div className="hero-container">
+        <video className="hero-vid" src={heroVidZoomed} autoPlay muted loop />
+      </div>
     </>
   );
 };
