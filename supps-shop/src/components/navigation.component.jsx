85f92b0ffc62f665414d7e7a745832343cc3f41f
Tayler Waddle <taylerwaddle@gmail.com>
2024-08-11 17:39:03 -0600

    altered navigation layout

--- supps-shop/src/components/navigation.component.jsx
+++ supps-shop/src/components/navigation.component.jsx
@@ -5,17 +5,20 @@ const Navigation = () => {
   return (
     <Fragment>
       <div className="z-50 flex h-20 w-full items-center justify-between bg-zinc-200">
-        <div className="ml-6 w-60">
+        <div className="ml-6">
           <h1 className="text-4xl font-normal text-black">PURE APEX</h1>
         </div>
 
-        <div className="flex items-center justify-between">
-          <Link className="ml-6 mr-6 text-2xl font-normal">Pre-workout</Link>
-          <Link className="ml-6 mr-6 text-2xl font-normal">Protein</Link>
-          <Link className="ml-6 mr-6 text-2xl font-normal">Creatine</Link>
+        {/* <div className="justify-self-end">
+          <Link className="ml-6 mr-6 text-2xl font-normal">Shop</Link>
+        </div> */}
+        <div className="flex justify-center">
+          <Link className="mx-6 text-2xl font-normal">Shop</Link>
+          <Link className="mx-6 text-2xl font-normal">Cart</Link>
+          <Link className="mx-6 text-2xl font-normal">Account</Link>
         </div>
 
-        <div className="-mr-8 flex w-60 justify-center">
+        {/* <div className="-mr-8 flex w-60 justify-center">
           <Link className="ml-6">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -38,7 +41,7 @@ const Navigation = () => {
               <path d="M240-80q-33 0-56.5-23.5T160-160v-480q0-33 23.5-56.5T240-720h80q0-66 47-113t113-47q66 0 113 47t47 113h80q33 0 56.5 23.5T800-640v480q0 33-23.5 56.5T720-80H240Zm0-80h480v-480h-80v80q0 17-11.5 28.5T600-520q-17 0-28.5-11.5T560-560v-80H400v80q0 17-11.5 28.5T360-520q-17 0-28.5-11.5T320-560v-80h-80v480Zm160-560h160q0-33-23.5-56.5T480-800q-33 0-56.5 23.5T400-720ZM240-160v-480 480Z" />
             </svg>
           </Link>
-        </div>
+        </div> */}
       </div>
       <Outlet />
     </Fragment>
