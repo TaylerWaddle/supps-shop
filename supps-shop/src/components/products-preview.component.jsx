6d73a0fb8956877211b7ec749e48a01813d63457
Tayler Waddle <taylerwaddle@gmail.com>
2024-08-13 22:43:37 -0600

    added header for products-preview

--- supps-shop/src/components/products-preview.component.jsx
+++ supps-shop/src/components/products-preview.component.jsx
@@ -3,8 +3,13 @@ import ProductCard from "./product-card.component";
 const ProductsPreview = () => {
   return (
     <div className="h-fit w-full bg-zinc-200 text-white">
+      <div className="flex items-center justify-center">
+        <h1 className="pt-16 text-4xl font-semibold text-black">
+          Most Popular Products
+        </h1>
+      </div>
       {/* Product Cards Container */}
-      <div className="flex justify-between p-32">
+      <div className="flex justify-between px-32 py-16">
         <ProductCard />
       </div>
     </div>
