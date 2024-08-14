c9863729544d7b43f77a823f4c9a4cc1b0d1fbc4
Tayler Waddle <taylerwaddle@gmail.com>
2024-08-13 23:36:57 -0600

    changed contact-us styling

--- supps-shop/src/components/contact-us.component.jsx
+++ supps-shop/src/components/contact-us.component.jsx
@@ -1,33 +1,41 @@
 const ContactUs = () => {
   return (
-    <div className="flex h-fit w-full items-center justify-center bg-zinc-200 pb-32 pt-32">
-      {/* Contact Us Header */}
-      <div className="flex h-96 w-1/4 items-center justify-center rounded-l-3xl bg-zinc-900">
-        <span className="text-4xl text-yellow-500">Contact Us</span>
+    <div className="flex h-fit w-full items-center justify-center bg-zinc-800 py-32">
+      <div className="flex h-fit w-fit flex-col items-center justify-evenly rounded-3xl bg-zinc-900">
+        <span className="py-16 text-4xl text-yellow-500 sm:px-24 md:px-48">
+          Contact Us
+        </span>
+        <form className="flex h-fit w-1/4 flex-col items-center justify-center rounded-r-3xl bg-zinc-900 pb-16">
+          <input
+            type="text"
+            placeholder="First Name"
+            className="mb-3 block w-80 rounded rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 sm:text-sm sm:leading-6"
+          ></input>
+          <input
+            type="text"
+            placeholder="Last Name"
+            className="mb-3 block w-80 rounded rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 sm:text-sm sm:leading-6"
+          ></input>
+          <input
+            type="email"
+            placeholder="Email"
+            className="mb-3 block w-80 rounded rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 sm:text-sm sm:leading-6"
+          ></input>
+          <textarea
+            type="textfield"
+            placeholder="Questions or Concerns"
+            className="mb-3 block min-h-32 w-80 rounded rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 sm:text-sm sm:leading-6"
+          ></textarea>
+          <div className="flex w-80 justify-center">
+            <button
+              type="submit"
+              className="text-md mt-1.5 w-full rounded-md bg-yellow-500 px-3 py-1.5 font-semibold leading-6 text-black shadow-sm hover:bg-yellow-500/85 hover:text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+            >
+              Submit Request
+            </button>
+          </div>
+        </form>
       </div>
-      {/* Form */}
-      <form className="flex h-3/4 h-96 w-1/4 flex-col items-center justify-center rounded-r-3xl bg-zinc-400 bg-opacity-50">
-        <input
-          type="text"
-          placeholder="First Name"
-          className="mb-3 w-80 rounded"
-        ></input>
-        <input
-          type="text"
-          placeholder="Last Name"
-          className="mb-3 w-80 rounded"
-        ></input>
-        <input
-          type="email"
-          placeholder="Email"
-          className="mb-3 w-80 rounded"
-        ></input>
-        <textarea
-          type="textfield"
-          placeholder="Questions or Concerns"
-          className="mb-3 min-h-40 w-80 rounded"
-        ></textarea>
-      </form>
     </div>
   );
 };
